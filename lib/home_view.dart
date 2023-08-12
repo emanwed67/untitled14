89872d612cef4f02f18647e3eab47a83c4e76312
eman wed <emanwed67@gmail.com>
2023-08-13 02:04:17 +0300

    update project

--- lib/home_view.dart
+++ lib/home_view.dart
@@ -22,17 +22,31 @@ class HomeView extends StatelessWidget {
                   SizedBox(height:4,),
                   Text("BEAUTIFUL",style:TextStyle(fontSize: 28,
                       fontWeight:FontWeight.w400,color:Color(0xff242424)  ) ,),
+                  SizedBox(height: 8,)
+                  
                 ]
             ),
           )
       ),
       body:GridView.count(crossAxisCount:2,
+        childAspectRatio: 157/253,
         children: [
           Padding(padding:EdgeInsets.all(8.0),
             child:Container(
-              child:Row(
+              child:Column(
                 children: [
-                  Image.asset('assets/images/photo_1.png')
+                  Stack(children: [Image.asset('assets/images/photo_1.png'),
+                    Positioned(
+                      left:120,
+                        bottom: 8,
+                        child: Image.asset('assets/images/Frame 19.png'))
+                  ]),
+                  SizedBox(height:8 ,),
+                  Text('Black Simple Lamp',
+                    style:TextStyle(color:Color(0xff606060),fontWeight:FontWeight.w400  ) ,),
+                  SizedBox(height: 10,),
+                  Text(' \$ 12.00',style:
+                  TextStyle(fontWeight: FontWeight.w700,color:Color(0xff303030) ) ,)
                 ],
               ) ,
               decoration: BoxDecoration(
@@ -42,9 +56,25 @@ class HomeView extends StatelessWidget {
             ) ,
           ),
           Padding(padding:EdgeInsets.all(8.0),
-            child:Container(child:Column(
+            child:Container(
+              child:Column(
               children: [
-                Image.asset('assets/images/photo_2.png'),
+                Stack(children: [Image.asset('assets/images/photo_2.png'),
+                  Positioned(
+                    left:120 ,
+                      bottom: 8,
+                      child:
+                  Image.asset('assets/images/Frame 19.png')
+                  )
+                ],
+
+                ),
+                SizedBox(height: 8,),
+                Text('Minimal Stand',
+                  style:TextStyle(fontWeight:FontWeight.w400,color:Color(0xff606060)  ),),
+                SizedBox(height:10 ,),
+                Text('\$ 25.00',
+                  style:TextStyle(fontWeight: FontWeight.w700,color:Color(0xff303030) ) ,)
               ],
             ) ,
               decoration:BoxDecoration(
@@ -55,9 +85,22 @@ class HomeView extends StatelessWidget {
           ),
            Padding(padding:EdgeInsets.all(8.0),
              child:Container(
-               child:Row(
+               child:Column(
                  children: [
-                   Image.asset('assets/images/photo_3.png')
+                   Stack(children: [Image.asset('assets/images/photo_3.png'),
+                     Positioned(
+                       left:120 ,
+                         bottom: 8,
+                         child: Image.asset('assets/images/Frame 19.png'))
+                   ]),
+                   SizedBox(height: 8,),
+                   Text('Coffee Chair',
+                     style:TextStyle(fontWeight:FontWeight.w400 ,
+                         color:Color(0xff606060),fontSize: 14  ),),
+                   SizedBox(height: 10,),
+                   Text('\$ 20.00',
+                     style:TextStyle(fontWeight:FontWeight.w700,
+                         fontSize: 14,color: Color(0xff303030) ) ,)
                  ],
                ) ,
             decoration:BoxDecoration(
@@ -68,9 +111,20 @@ class HomeView extends StatelessWidget {
            ),
           Padding(padding:EdgeInsets.all(8.0),
             child:Container(
-              child: Row(
+              child: Column(
                 children: [
-                  Image.asset('assets/images/photo_4.png')
+                  Stack(children: [Image.asset('assets/images/photo_4.png'),
+                    Positioned(
+                      left: 120,
+                        bottom: 8,
+                        child:Image.asset('assets/images/Frame 19.png'))
+                  ]),
+                  SizedBox(height: 8,),
+                  Text('Simple Desk',
+                    style:TextStyle(fontWeight: FontWeight.w400 ,
+                        color:Color(0xff606060),fontSize:14  ) ,),
+                  SizedBox(height: 10,),
+                  Text('\$ 50.00',style:TextStyle(fontWeight:FontWeight.w700,fontSize: 14,color:Color(0xff303030)  ) ,)
                 ],
               ),
               decoration:BoxDecoration(
@@ -79,7 +133,6 @@ class HomeView extends StatelessWidget {
               ) ,
             ) ,
           ),
-
         ],
       ) ,
     );
